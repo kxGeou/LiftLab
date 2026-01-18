40af4f883784eb250fb67d7d777b008da0896d38
kxGeou <grzegorzm.work@gmail.com>
2026-01-18 22:16:12 +0100

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,11 @@ import Footer from '../UI/Footer'
 
 function Layout({children}: {children: React.ReactNode}) {
   return (
-    <main className='min-h-screen flex flex-col items-center bg-[url(./bg.jpg)] bg-cover main-font  text-white '>
+    <main
+  style={{ backgroundImage: "url('/bg.jpg')" }}
+  className="min-h-screen flex flex-col items-center bg-cover main-font text-white"
+>
+
       <div className='min-h-screen backdrop-blur-3xl bg-gray-950/40 w-full p-2 md:p-4'>
         <div className='max-w-400 mx-auto'>
           <Header></Header>
